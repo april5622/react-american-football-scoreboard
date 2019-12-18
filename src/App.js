a7ba5d0e7b71681a38e4bb29119936191875278f
April Ma <54913304+april5622@users.noreply.github.com>
2019-12-18 15:55:24 -0800

    MVP finished

--- src/App.js
+++ src/App.js
@@ -5,7 +5,25 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [value, setValue] = useState();
+  const [homescore, setHomescore] = useState(32);
+  const [awayscore, setAwayscore] = useState(32);
+
+  const homeTouchdown = e => {
+    setHomescore(homescore + 7);
+  };
+
+  const homeFieldgoal = e => {
+    setHomescore(homescore + 3);
+  };
+
+  const awayTouchdown = e => {
+    setAwayscore(awayscore + 7);
+  };
+
+  const awayFieldgoal = e => {
+    setAwayscore(awayscore + 3);
+  };
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -15,12 +33,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+          <div className="home__score">{homescore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayscore}</div>
           </div>
         </div>
         <BottomRow />
@@ -28,12 +46,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={homeTouchdown}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={homeFieldgoal}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={awayTouchdown}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={awayFieldgoal}>Away Field Goal</button>
         </div>
       </section>
     </div>
@@ -41,3 +59,4 @@ function App() {
 }
 
 export default App;
+
